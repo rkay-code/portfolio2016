b4a50f74963150ba9b2d03f11095c21f22dcca52
reneepadgham <renee@padgham.com>
2016-07-12 13:46:58 -0700

    polish tweaks

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
 
+  $('.nav a').on('click', function() {
+      $('.navbar-collapse').collapse('hide');
+  });
+
+  $("#name-input").keydown(function(event){
+    if(event.keyCode == 13){
+        $("#name-sbmt").click();
+    }
+});
 
 });
