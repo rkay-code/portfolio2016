7a5e2283e590c7095511d2c624f86725d22b4c6d
reneepadgham <renee@padgham.com>
2016-07-21 18:20:54 -0700

    new-layout

--- scripts/controller.js
+++ scripts/controller.js
@@ -132,3 +132,21 @@ portfolioApp.controller('workCtrl', ['$scope', '$stateParams', '$http', '$locati
     });
   }
 ]);
+
+portfolioApp.directive('scrollOnClick', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, $elm, attrs) {
+      var idToScroll = attrs.href;
+      $elm.on('click', function() {
+        var $target;
+        if (idToScroll) {
+          $target = $(idToScroll);
+        } else {
+          $target = $elm;
+        }
+        $("body").animate({scrollTop: $target.offset().top}, "slow");
+      });
+    }
+  }
+});
