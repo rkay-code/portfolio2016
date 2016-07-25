a0486b6dacbe23a718c24efcdffa39cfef5b7185
Geoffrey Vedernikoff <yefim323@gmail.com>
2016-07-24 23:35:34 -0700

    Add backbone router

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,120 @@
-$(document).ready(function() {
+var workTemplate = _.template([
+  '<div class="col-md-6 col-md-offset-0 col-sm-10 col-sm-offset-1 proj-wrapper">',
+    '<a href="/work/<%= work.id %>">',
+      '<img ng-src="<%= work.img %>" />',
+    '</a>',
+    '<div class="brief-details">',
+      '<h3><%= work.title %></h3>',
+      '<h4><%= work.responsibility %></h4>',
+      '<a href="#/work/<%= work.id %>">',
+        '<button class="view-proj" ng-if="work.type"><%= work.type %></button>',
+      '</a>',
+    '</div>',
+  '</div>'
+].join(''));
 
-  $('.nav a').on('click', function() {
-    $('.navbar-collapse').collapse('hide');
-  });
+var work = [
+  {
+    id: 1,
+    img: '',
+    title: 'Boss',
+    responsibility: 'More boss',
+    type: 'test'
+  },
+  {
+    id: 2,
+    img: '',
+    title: 'Boss',
+    responsibility: 'More boss',
+    type: 'test'
+  },
+  {
+    id: 3,
+    img: '',
+    title: 'Boss',
+    responsibility: 'More boss',
+    type: 'test'
+  }
+];
 
-  $(".nav a").on("click", function(){
-   $(".nav").find(".active").removeClass("active");
-   $(this).parent().addClass("active");
+
+var Router = Backbone.Router.extend({
+  initialize: function(options) {
+    this.$app = options.app;
+    this.templates = options.templates;
+
+    // What's currently rendered
+    this.currentTemplate = undefined;
+  },
+
+  routes: {
+    '': 'index',
+    'work/:id': 'work',
+    'work': 'index',
+    'desk': 'index',
+    'room': 'index',
+    'contact': 'index'
+  },
+
+  index: function() {
+    if (this.currentTemplate === 'index') {
+      return;
+    }
+
+    this.currentTemplate = 'index';
+
+    this.$app.html(this.templates.index);
+
+    var html = work.map(function(w) {
+      return workTemplate({work: w});
+    });
+
+    $('.proj-brief-wrapper').html(html.join(''));
+
+    $('body').scrollspy({
+      target: '#my-nav'
+    });
+  },
+
+  work: function(id) {
+    if (this.currentTemplate === 'work-detail') {
+      return;
+    }
+
+    this.currentTemplate = 'work-detail';
+
+    var html = _.template(this.templates.work)({
+      prevWork: '',
+      nextWork: '',
+      work: {
+        id: id,
+        briefImg: '',
+        title: 'My Work',
+        tagline: 'My tagline',
+        responsibility: 'All of it',
+        problemImg1: '',
+        problemImg2: '',
+        problemImg3: '',
+        researchImg1: '',
+        researchImg2: '',
+        designImg1: '',
+        designImg2: '',
+        designImg3: ''
+      }
+    });
+
+    this.$app.html(html);
+  }
 });
 
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 200) {
-      $('.navbar-scroll').fadeIn(500);
+$(document).ready(function() {
+  var router = new Router({
+    app: $('#app'),
+    templates: {
+      index: $('#index-content').html(),
+      work: $('#work-detail-content').html()
     }
   });
 
-$('.body-main').scrollspy({ target: '#spy-nav' })
+  Backbone.history.start();
 });
